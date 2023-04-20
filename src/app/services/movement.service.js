b85caeacba29d1a3781143c0d086c7cd505be1ea
Julio Cesar Becerra Urbina <julioc_m18@hotmail.com>
2023-04-20 01:35:01 -0500

    fix dockerization

--- src/app/services/movement.service.js
+++ src/app/services/movement.service.js
@@ -1,17 +1,6 @@
-const redis = require('redis')
+
 const movementRepository = require('../repositories/movement.repository')
 
-const client = redis.createClient({
-    url: process.env.DB_REDIS_URL,
-    tls: {
-        rejectUnauthorized: false
-    },
-    legacyMode: true
-})
-client.connect();
-client.on('error', function (err) {
-    console.log('Error ' + err)
-})
 
 const movementService = {
     getInvoice: async () => {
